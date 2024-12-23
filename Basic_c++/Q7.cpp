1fd25762282dfd44dc82ee840b094f07c79b4b91
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-24 00:14:16 +0530

    Create Q7.cpp

---
+++ Basic_c++/Q7.cpp
@@ -0,0 +1,19 @@
+/* (7)print the digits of given no in reverse using while loop*/
+#include <iostream> 
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    cout << "Digits of the number in reverse order are:" << endl;
+
+    while (n > 0) {
+        int last_digit = n % 10; 
+        cout << last_digit ; 
+        n /= 10; 
+    }
+
+    return 0;
+}
