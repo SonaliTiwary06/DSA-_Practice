73231c670d022ff71ae8d8907f25b5dc38069021
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-24 00:04:01 +0530

    Create Q2.cpp

---
+++ Basic_c++/Q2.cpp
@@ -0,0 +1,19 @@
+/* (2)Enter cost of 3 items from the user (using float data type) - a pencil, a
+pen and an eraser. You have to output the total cost of the items back to the user as their bill.*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    float pencil;
+    float pen;
+    float eraser;
+    cout<<"Cost of pencil is"<<endl;
+    cin>>pencil;
+    cout<<"Cost of pen is"<<endl;
+    cin>>pen;
+    cout<<"Cost of eraser is"<<endl;
+    cin>>eraser; 
+    float Bill = pencil + pen + eraser;
+    cout<<"Bill of items is"<<Bill;
+    return 0;
+}
