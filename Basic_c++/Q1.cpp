9aded918300d117c1622670c02827fef5323badf
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-23 23:55:42 +0530

    Create Q1.cpp

---
+++ Basic_c++/Q1.cpp
@@ -0,0 +1,12 @@
+/*(1)In a program, input the side of a square. You have to output the area of the square*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int side ;
+    cout<<"Side of square is\n";
+    cin>>side;
+    int Area = side*side;
+    cout<<"Area of square is"<<Area;
+    return 0;
+}
