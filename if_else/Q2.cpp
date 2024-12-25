665097cc3ac29685633c8d29adc9f7fa453bc6b0
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-25 08:19:40 +0530

    Create Q2.cpp

---
+++ if_else/Q2.cpp
@@ -0,0 +1,18 @@
+/* (2)Write a C++ program that takes a year from the user and print whether
+that year is a leap year or not.*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Year;
+    cout << "Enter the year: ";
+    cin >> Year;
+
+    if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0)) {
+        cout << "This is a leap year." << endl;
+    } else {
+        cout << "This is not a leap year." << endl;
+    }
+
+    return 0;
+}
