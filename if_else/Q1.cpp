75d9b943da3789bc51cd10601b0b4013668b24aa
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-25 08:18:24 +0530

    Create Q1.cpp

---
+++ if_else/Q1.cpp
@@ -0,0 +1,22 @@
+/* (1)Write a C++ program to get a number from the user and print whether
+it's positive, negative or zero.*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Given no is"<<endl;
+    cin>>a;
+    if(a<0){
+        cout<<"The no is negative"<<endl;
+    }
+    else{
+        if(a>0){
+            cout<<"The no is positive"<<endl;
+        }
+        else{
+            cout<<"The no is zero"<<endl;
+        }
+    }
+    return 0;
+}
