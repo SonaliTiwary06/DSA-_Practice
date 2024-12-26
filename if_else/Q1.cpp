ad89bcdac7afaac124be372a2d643b2e6c8ac8fb
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-27 00:57:31 +0530

    Update Q1.cpp

--- if_else/Q1.cpp
+++ if_else/Q1.cpp
@@ -1,4 +1,4 @@
-/* (1)Write a C++ program to get a number from the user and print whether
+/* Write a C++ program to get a number from the user and print whether
 it's positive, negative or zero.*/
 #include <iostream>
 using namespace std;
