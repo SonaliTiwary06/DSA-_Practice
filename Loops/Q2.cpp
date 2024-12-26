f3ad142c31af3bbf50684fd4d03df21fdae7e623
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 12:47:52 +0530

    Create Q2.cpp

---
+++ Loops/Q2.cpp
@@ -0,0 +1,13 @@
+/* (2)print numbers from 1 to N*/
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    int a;
+    cout<<"Enter the last no:"<<endl;
+    cin>>N;
+    for(a=0;a<=N;a=a+1){
+        cout<<a<<endl;
+    }
+    return 0;
+}
