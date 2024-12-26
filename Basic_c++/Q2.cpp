ee027887ab0a7dd8659d7207a103a481411437b5
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-27 00:58:47 +0530

    Update Q2.cpp

--- Basic_c++/Q2.cpp
+++ Basic_c++/Q2.cpp
@@ -1,4 +1,4 @@
-/* (2)Enter cost of 3 items from the user (using float data type) - a pencil, a
+/* Enter cost of 3 items from the user (using float data type) - a pencil, a
 pen and an eraser. You have to output the total cost of the items back to the user as their bill.*/
 #include <iostream>
 using namespace std;
