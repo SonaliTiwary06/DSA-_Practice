08feac8f4f0f068baa2b81af1e10eace8f05824c
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-27 00:57:56 +0530

    Update Q2.cpp

--- if_else/Q2.cpp
+++ if_else/Q2.cpp
@@ -1,4 +1,4 @@
-/* (2)Write a C++ program that takes a year from the user and print whether
+/* Write a C++ program that takes a year from the user and print whether
 that year is a leap year or not.*/
 #include <iostream>
 using namespace std;
