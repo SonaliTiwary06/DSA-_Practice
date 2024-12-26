71e8705da7983f0dbe27a8ada67ed92170895084
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-27 00:27:16 +0530

    Create Q13.cpp

---
+++ Loops/Q13.cpp
@@ -0,0 +1,16 @@
+/* (13) WAP to show numbers entered by user except multiple of 10*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    
+    for(;;){
+        cout<<" Enter a no:"<<endl;
+        cin>>n;
+        if (n%10==0){
+           continue;
+    }
+        cout<<"You entered :"<<n<<endl;
+    }
+    return 0;
+}
