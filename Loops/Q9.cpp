32205c4de4d48d42ba330fe7bf057bb4c51d88ea
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 12:59:34 +0530

    Create Q9.cpp

---
+++ Loops/Q9.cpp
@@ -0,0 +1,19 @@
+/* (9) print sum of odd digits of entered no using while loop*/
+#include<iostream>
+using namespace std;
+int main(){
+    int sum = 0;
+    int n;
+    cout<<"Enter the no"<<endl;
+    cin>>n;
+
+    while(n>0){
+        int last_digit = n%10;
+        if(last_digit%2!=0){
+            sum = sum +last_digit;
+        }
+        n=n/10;
+    }
+    cout<<"The sum of odd digit of no is"<<sum<<endl;
+    return 0;
+}
