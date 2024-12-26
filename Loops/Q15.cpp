4a914c09e4504db6afcdce8e2a25729e579d9bb7
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-27 00:32:57 +0530

    Create Q15.cpp

---
+++ Loops/Q15.cpp
@@ -0,0 +1,20 @@
+/* (14) check if a no is prime or not using for loop*/   
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 7;
+    bool isPrime = true;
+    for(int i=2; i<=n-1;i++){
+        if(n%i==0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime == true){
+        cout<<"number is Prime"<<endl;
+    }
+    else{
+        cout<<"number is not Prime"<<endl;
+    }
+    return 0;
+}
