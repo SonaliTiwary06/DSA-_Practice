d247675ae8869ed13aa066c6bbfdd6909a79baa9
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 13:08:30 +0530

    Create Q12.cpp

---
+++ Loops/Q12.cpp
@@ -0,0 +1,18 @@
+/* (12)WAP where user can keep entering numbers till they enter multiple of 10 using for loop */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    for (;;) {  
+        cout << "Enter a number: ";
+        cin >> n;
+        
+        if (n % 10 == 0) {
+            break;  
+        }
+        cout<<"The entered no is:"<<n<<endl;
+    }
+
+    return 0;
+}
