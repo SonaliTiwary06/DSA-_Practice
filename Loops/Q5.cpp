bc967b9d71075afe1878e8543a1189b21cc1433e
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 12:55:00 +0530

    Create Q5.cpp

---
+++ Loops/Q5.cpp
@@ -0,0 +1,15 @@
+/* (5)print no from N to 1 using for loop*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter the number: " << endl;
+    cin >> N;
+
+    for (int s = N; s >= 1; s--) {
+        cout << s << endl;
+    }
+    
+    return 0;
+}
