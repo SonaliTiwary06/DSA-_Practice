1b6afaeb6e3e8f5ecc4e90ae4490e952250580ae
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 13:00:59 +0530

    Create Q10.cpp

---
+++ Loops/Q10.cpp
@@ -0,0 +1,18 @@
+/* (10) reverse the no and print the result*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int res = 0;  
+    int n;        
+    cout << "Enter the number:" << endl;
+    cin >> n;
+    while (n > 0) {
+        int last_digit = n % 10;   
+        res = res * 10 + last_digit; 
+        n = n / 10;               
+    }
+
+    cout << "Reversed number: " << res << endl;
+    return 0;
+}
