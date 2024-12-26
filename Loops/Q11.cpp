a622d84e388ef2cb9c4a65d0f4464f1e77dac7dc
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 13:02:11 +0530

    Create Q11.cpp

---
+++ Loops/Q11.cpp
@@ -0,0 +1,16 @@
+/* (11)write a program where user can keep entering no till they enter a multiple of 10*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    while(n>0){
+        cout<<"Enter a no:"<<endl;
+        cin>>n;
+        if(n%10==0){
+            break;
+
+        }
+        cout<<"the entered no is:"<<n<<endl;
+    }
+    return 0;
+}
