b1db0b30bdcb7143b86ac44a112c4c4ba429d623
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 12:57:20 +0530

    Create Q7.cpp

---
+++ Loops/Q7.cpp
@@ -0,0 +1,14 @@
+/* (7) print sum of digits of no 10829 using while loop*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10829;
+    int sum = 0;
+    while(n>0){
+        int last_digit = n%10;
+        sum = sum + last_digit;
+        n = n/10;
+    }
+    cout<<"the sum of digits of no is"<<sum<<endl;
+    return 0;
+}
