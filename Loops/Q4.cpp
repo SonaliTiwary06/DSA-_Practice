89d440ca822ca1d0df187b50a61b61c35dc181d6
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 12:52:51 +0530

    Create Q4.cpp

---
+++ Loops/Q4.cpp
@@ -0,0 +1,13 @@
+/* (4) print square pattern using for loop     * * * *
+                                               * * * *
+                                               * * * *
+                                               * * * *   */
+#include <iostream>
+using namespace std;
+int main(){
+    int s;
+    for(s=0;s<4;s++){
+        cout<<"* * * *"<<endl;
+    }
+    return 0;
+}
