28ba74e759e91faed24eb18dcc5bc02e6bc8bc1a
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-27 00:34:23 +0530

    Create Q16.cpp

---
+++ Loops/Q16.cpp
@@ -0,0 +1,22 @@
+/*  check if a no is prime or not using while loop*/ 
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 9;
+    bool isPrime = true;
+    int a=2;
+    while(a<n){
+        if(n%a==0){
+        isPrime = false;
+        break;
+        }
+        a++;
+    }
+    if (isPrime==true){
+    cout<<"number is Prime"<<endl;
+    }
+    else{
+    cout<<"number is not Prime"<<endl;
+    }
+    return 0;
+}
