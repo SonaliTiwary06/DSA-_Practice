6ab724bb93d260b860e40523f0f03aa8ea136738
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-26 12:58:07 +0530

    Create Q8.cpp

---
+++ Loops/Q8.cpp
@@ -0,0 +1,16 @@
+/* (8) print sum of digits of no using while loop*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no:"<<endl;
+    cin>>n;
+    int sum=0;
+    while(n>0){
+        int last_digit=n%10;
+        sum=sum+last_digit;
+        n=n/10;
+    }
+    cout<<"sum of digits of no is:"<<sum<<endl;
+    return 0;
+}
