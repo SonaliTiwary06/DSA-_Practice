38c268c309b073056cbb3862bd2dd6deae0d9ba4
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-28 17:53:38 +0530

    Create Q3.cpp

---
+++ Patterns/Q3.cpp
@@ -0,0 +1,15 @@
+/* print star pattern *
+                         * *
+                         * * *
+                         * * * *    */
+#include <iostream>
+using namespace std;
+int main()  {
+    for(int x=1; x<=4; x++){
+        for(int y=1; y<=x; y++){
+            cout<<"* ";                                              
+        }
+        cout<<endl;
+    }
+    return 0;
+}
