1da18463a4f49bc83f0e4436be8ca068cbf75c4b
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-28 17:55:50 +0530

    Create Q5.cpp

---
+++ Patterns/Q5.cpp
@@ -0,0 +1,18 @@
+/* print inverted star pattern * * * *
+                                  * * *
+                                  * *
+                                  *      */
+#include <iostream>
+using namespace std;                                                             
+int main(){
+    int n;                                                                   
+    cout<<"Enter the value of n:"<<endl;                                     
+    cin>>n;                                                                  
+    for(int x=1; x<=n; x++){
+        for(int y=1; y<=(n-x+1); y++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
