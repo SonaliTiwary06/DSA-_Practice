cad0a31a10cb96fb61ea5858dedea799f49cdd8b
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-28 17:59:19 +0530

    Update Q5.cpp

--- Patterns/Q5.cpp
+++ Patterns/Q5.cpp
@@ -1,4 +1,4 @@
-/* print inverted star pattern * * * *
+/*    print inverted star pattern * * * *
                                   * * *
                                   * *
                                   *      */
