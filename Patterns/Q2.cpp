216fdb51a520a0e9634da9800856afa75b7758ca
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-29 21:59:32 +0530

    Update Q2.cpp

--- Patterns/Q2.cpp
+++ Patterns/Q2.cpp
@@ -1,4 +1,7 @@
-/*  print a same pattern above for n rows and n column*/ 
+/*  print a same pattern  1 1 1 1
+                          2 2 2 2
+                          3 3 3 3
+                          4 4 4 4 for n rows and n column*/ 
 #include <iostream>
 using namespace std;
 int main(){
