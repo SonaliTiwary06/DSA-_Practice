7046b5aedafb813745c6c73fd316a24fe19eb9e8
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-31 22:00:43 +0530

    Update WAF_to_find_difference.cpp

--- Functions/WAF_to_find_difference.cpp
+++ Functions/WAF_to_find_difference.cpp
@@ -1,1 +1,12 @@
 /*  WAF to find difference of 2 numbers x , y*/
+#include <iostream>
+using namespace std;
+int diff (int x=6, int y=2){
+    int diff = x - y;
+    return diff;
+}
+int main(){
+    int d = diff();
+    cout<<"diff :"<<d<<endl;
+    return 0;
+}
