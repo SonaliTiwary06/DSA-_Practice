75f042374cf30699418628936f374ddf2a5063fc
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-31 22:02:14 +0530

    Create WAF_to_find_product.cpp

---
+++ Functions/WAF_to_find_product.cpp
@@ -0,0 +1,11 @@
+/*  WAF to find product of 2 numbers x , y*/
+#include <iostream>
+using namespace std;
+int val (int x, int y=3){       
+     return  x*y;
+}
+int main(){
+            
+    cout<<val(2)<<endl;
+    return 0;
+}
