ef048428e33a09cd5f0f124682353d29d4f1a055
SONALI TIWARY <sonali.dtg1@gmail.com>
2024-12-31 21:57:26 +0530

    Update WAF_to_find_sum.cpp

--- Functions/WAF_to_find_sum.cpp
+++ Functions/WAF_to_find_sum.cpp
@@ -1,1 +1,12 @@
 /*  WAF to find sum of 2 numbers x , y */ 
+#include <iostream>
+using namespace std;
+int sum (int x, int y){     // x , y are parameters   
+    int sum = x+y;
+    return sum;
+}
+int main(){
+    int s = sum(2,4);        // 2 , 4 are arguments
+    cout<<"sum:"<<s<<endl;
+    return 0;
+}
