278a060e3e8111f60e460c9f4543c1d751556303
SONALI TIWARY <sonali.dtg1@gmail.com>
2025-01-01 17:39:35 +0530

    Update WAF_to get_factorial.cpp

--- Functions/WAF_to get_factorial.cpp
+++ Functions/WAF_to get_factorial.cpp
@@ -1,1 +1,20 @@
 /*   WAF to print factorial of number n*/
+#include <iostream>
+using namespace std;
+int factorial (int n){
+    int fact=1;
+    for(int x=1; x<=n; x++){
+        fact=fact*x;
+    }                                                                 
+    cout<<"factoraial of "<< n <<" is :"<<fact<<endl;
+    return fact;
+}
+int main(){
+    factorial(2);
+    factorial(6);
+    factorial(9);
+    factorial(11);
+    factorial(21);
+    factorial(19);
+    return 0;
+}
