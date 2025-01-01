2b9467c225f6f4d5d5e19fc9c287d2392206dfeb
SONALI TIWARY <sonali.dtg1@gmail.com>
2025-01-01 17:44:11 +0530

    Update WAF_to_print_all_prime_from_2_to_n.cpp

--- Functions/WAF_to_print_all_prime_from_2_to_n.cpp
+++ Functions/WAF_to_print_all_prime_from_2_to_n.cpp
@@ -1,1 +1,21 @@
 /*  print all prime in a range from 2 to n */
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+    for(int x=2; x<=n-1; x++){
+        if(n%x==0){
+            return false;
+        }
+    }
+    return true;
+}
+void allPrime(int n){
+    for(int x=2; x<=n; x++)
+    if(isPrime(x)){
+        cout<<x<<"  ";
+    }
+}
+int main(){
+    allPrime(20);
+    return 0;
+}
